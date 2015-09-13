ec25e6f8720119325a3f034a230579f9f5a0162b
Ben Hong <bencodezen@gmail.com>
2015-09-13 18:17:42 -0400

    [FR] Add a controller and refactor out variables

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-var ngMadLibApp = angular.module('ngMadLibApp', []);
+var ngMadLibApp = angular.module('ngMadLibApp', [])
+	.controller("ngMadLibCtrl", function($scope) {
+		$scope.femaleName = '';
+		$scope.jobTitle = '';
+		$scope.tediousTask = '';
+		$scope.dirtyTask = '';
+		$scope.celebrity = '';
+		$scope.uselessSkill = '';
+		$scope.adjective = '';
+		$scope.obnoxiousCelebrity = '';
+		$scope.hugeNumber = '';
+	});
