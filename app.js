e7e96d854c052e020d87e526d048ce84f65260ae
Ben Hong <bencodezen@gmail.com>
2015-09-27 20:34:16 -0400

    [F] Add animation and change display logic for content blocks

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-var ngMadLibApp = angular.module('ngMadLibApp', [])
-	.controller("ngMadLibCtrl", function($scope) {
-		$scope.name = '';
-		$scope.gender = '';
-		$scope.jobTitle = '';
-		$scope.tediousTask = '';
-		$scope.dirtyTask = '';
-		$scope.celebrity = '';
-		$scope.uselessSkill = '';
-		$scope.adjective = '';
-		$scope.obnoxiousCelebrity = '';
-		$scope.hugeNumber = '';
-		$scope.submit = function() {
-			if ($scope.madLibForm.$valid) {
-				console.log("Valid!");
-				document.getElementById("user-input").style.display = 'none';
-				document.getElementById("content").style.display = 'block';
-			} else {
-				console.log("Invalid!");
-			}
-		};
-		$scope.restart = function() {
-			document.getElementById("content").style.display = 'none';
-			document.getElementById("user-input").style.display = 'block';
+var ngMadLibApp = angular.module('ngMadLibApp', ['ngAnimate']);
+
+ngMadLibApp.controller("ngMadLibCtrl", function($scope) {
+	$scope.name = '';
+	$scope.gender = '';
+	$scope.jobTitle = '';
+	$scope.tediousTask = '';
+	$scope.dirtyTask = '';
+	$scope.celebrity = '';
+	$scope.uselessSkill = '';
+	$scope.adjective = '';
+	$scope.obnoxiousCelebrity = '';
+	$scope.hugeNumber = '';
+	$scope.submit = function() {
+		if ($scope.madLibForm.$valid) {
+			console.log("Valid!");
+		} else {
+			console.log("Invalid!");
 		}
-	});
+	};
+	$scope.restart = function() {
+		$scope.madLibForm.$setPristine();
+	}
+});
